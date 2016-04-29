806fa2d5909e8b288706839312bbddaf8c438199
AjaxGb <ajaxgb@gmail.com>
2016-04-29 00:18:45 -0400

    Added first-letter index organization

--- db/buildindex.py
+++ db/buildindex.py
@@ -2,27 +2,33 @@ import sys, os, re
 from string import Template
 from collections import defaultdict
 
-notWord = re.compile("[^A-Za-z0-9]")
 def encodeWord(word):
-	response = ""
+	if word[0].isalpha():
+		folder = word[0]
+	else:
+		folder = "0"
+	encoded = ""
 	for char in word:
-		if notWord.match(char):
-			response += "-" + repr(ord(char)) + "-"
+		if char.isalnum():
+			encoded += char
 		else:
-			response += char
-	return response
+			encoded += "-" + repr(ord(char)) + "-"
+	return folder, encoded
 
 ### BEGIN MAIN FUNCTION ###
 
 chapI, pageI = 1, 1
 fileTemp = Template("ch$c/p$p.txt")
-index = defaultdict(list)
+index = defaultdict(lambda : defaultdict(list))
 while True:
 	while os.path.isfile(fileTemp.substitute(c=chapI, p=pageI)):
-		file = open(fileTemp.substitute(c=chapI, p=pageI))
-		for wordI, word in enumerate(file.read().split()):
-			index[encodeWord(word)].append((chapI, pageI, wordI + 1))
-		file.close()
+		with open(fileTemp.substitute(c=chapI, p=pageI)) as file:
+			wordI = 1
+			for line in file:
+				for word in line.split():
+					folder, encoded = encodeWord(word)
+					index[folder][encoded].append((chapI, pageI, wordI))
+					wordI += 1
 		pageI += 1
 	if pageI == 1:
 		break
@@ -30,41 +36,55 @@ while True:
 	chapI += 1
 	pageI = 1
 
-print("\nTotal words found:", len(index))
+print("\nTotal words found:", sum(len(folder) for folder in index.values()))
 
 os.makedirs("index", exist_ok=True)
 os.chdir("index")
-existingIndexFiles = os.listdir()
+existingIndexFiles = {}
+for folder in os.listdir():
+	if not os.path.isdir(folder):
+		print("Odd file found in index:", folder)
+	else:
+		existingIndexFiles[folder] = os.listdir(folder)
 removed = 0
-for file in existingIndexFiles:
-	if file[:-4] not in index.keys():
-		os.remove(file)
-		removed += 1
+for folder, files in existingIndexFiles.items():
+	if folder not in index:
+		for file in files:
+			os.remove(folder + "/" + file)
+			removed += 1
+		os.rmdir(folder)
+	else:
+		for file in files:
+			if file[:-4] not in index[folder]:
+				os.remove(folder + "/" + file)
+				removed += 1
+		
 print(removed, "file(s) culled")
 
 updated, created = 0, 0
-for word, positions in index.items():
-	fileContent = [",".join(str(x) for x in pos) for pos in positions]
-	fileName = word + ".txt"
-	if fileName in existingIndexFiles:
-		existingFile = open(fileName)
-		i = 0
-		for line in existingFile:
-			if i >= len(fileContent) or line.strip() != fileContent[i]:
+for folder in index:
+	for word, positions in index[folder].items():
+		fileContent = [",".join(str(x) for x in pos) for pos in positions]
+		fileName = word + ".txt"
+		if folder in existingIndexFiles and fileName in existingIndexFiles[folder]:
+			existingFile = open(folder + "/" + fileName)
+			i = 0
+			for line in existingFile:
+				if i >= len(fileContent) or line.strip() != fileContent[i]:
+					existingFile.close()
+					break
+				i += 1
+			else:
 				existingFile.close()
-				break
-			i += 1
+				if i == len(fileContent):
+					continue
+			updated += 1
 		else:
-			existingFile.close()
-			if i == len(fileContent):
-				continue
-		updated += 1
-	else:
-		created += 1
-	indexFile = open(fileName, mode="w")
-	for line in fileContent:
-		print(line, file=indexFile)
-	indexFile.close()
+			created += 1
+		os.makedirs(folder, exist_ok=True)
+		with open(folder + "/" + fileName, mode="w") as indexFile:
+			for line in fileContent:
+				print(line, file=indexFile)
 
 print(created, "file(s) created")
 print(updated, "file(s) updated")
